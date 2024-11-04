3ff8bfdc6619b4056f8fa5a1153ea62891c498b0
liyin <liyincode@gmail.com>
2024-11-04 09:39:21 +0800

    feat: implement router

---
+++ components.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export function Layout({ children }) {
+    const author = "YaYu";
+    return (
+        <html>
+        <head>
+            <title>My blog</title>
+            <script src="https://cdn.tailwindcss.com"></script>
+        </head>
+        <body className="p-5">
+        <nav className="flex items-center justify-center gap-10 text-blue-600">
+            <a href="/">Home</a>
+        </nav>
+        <main>{children}</main>
+        <Footer author={author} />
+        </body>
+        </html>
+    );
+}
+
+export function IndexPage({ slugs, contents }) {
+    return (
+        <section>
+            <h1>Blog List:</h1>
+            <div>
+                {slugs.map((slug, index) => (
+                    <section key={slug} className="mt-4">
+                        <a className="text-blue-600" href={"/" + slug}>{slug}</a>
+                        <article className="h-40 mt-5 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">{contents[index]}</article>
+                    </section>
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export function PostPage({ slug, content }) {
+    return (
+        <section>
+            <a className="text-blue-600" href={"/" + slug}>{slug}</a>
+            <article className="h-40 mt-5 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">{content}</article>
+        </section>
+    );
+}
+
+export function Footer({ author }) {
+    return (
+        <footer className="h-20 mt-5 flex-1 rounded-xl bg-cyan-500 text-white flex items-center justify-center">
+            (c) { author }, {new Date().getFullYear()}
+        </footer>
+    );
+}
--- generator.tsx
+++ generator.tsx
@@ -1,28 +1,27 @@
-import { readFile } from "fs/promises";
+import { readFile, readdir } from "fs/promises";
 import React from 'react';
 import { renderJSXToHTML } from './utils'
+import { Layout, IndexPage, PostPage } from './components'
 
-export async function htmlGenerator() {
-    const author = "John Doe";
-    const postContent = await readFile("./posts/hello.txt", "utf8");
+export async function htmlGenerator(url) {
+    const page = await matchRoute(url);
+    return renderJSXToHTML(<Layout>{page}</Layout>);
+}
 
-    let jsx = <html>
-    <head>
-        <title>My blog</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body className="p-5">
-    <nav className="flex items-center justify-center gap-10 text-blue-600">
-        <a href="/">Home</a>
-    </nav>
-    <article className="h-40 mt-5 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">
-        { postContent }
-    </article>
-    <footer className="h-20 mt-5 flex-1 rounded-xl bg-cyan-500 text-white flex items-center justify-center">
-        (c) { author }, {new Date().getFullYear()}
-    </footer>
-    </body>
-    </html>
-
-    return renderJSXToHTML(jsx);
+async function matchRoute(url) {
+    console.log('matchRoute', url)
+    if (url.pathname === "/") {
+        const files = await readdir("./posts");
+        const slugs = files.map((file) => file.slice(0, file.lastIndexOf(".")));
+        const contents = await Promise.all(
+            slugs.map((slug) =>
+                readFile("./posts/" + slug + ".txt", "utf8")
+            )
+        );
+        return <IndexPage slugs={slugs} contents={contents} />;
+    } else {
+        const slug = url.pathname.slice(1);
+        const content = await readFile("./posts/" + slug + ".txt", "utf8");
+        return <PostPage slug={slug} content={content} />;
+    }
 }

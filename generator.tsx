3d98e8b10f979b49d3d3b6104f843edaac1f77a4
liyin <liyincode@gmail.com>
2024-11-06 08:57:45 +0800

    feat: implement rsc

--- generator.tsx
+++ generator.tsx
@@ -1,10 +1,36 @@
 import { readFile, readdir } from "fs/promises";
 import React from 'react';
-import { renderJSXToHTML } from './utils'
+import { renderJSXToHTML, renderJSXToClientJSX, stringifyJSX } from './utils'
 import { Layout, IndexPage, PostPage } from './components'
 
 export async function htmlGenerator(url) {
-    return renderJSXToHTML(<Router url={url} />);
+    let jsx = <Router url={url} />
+    let html = await renderJSXToHTML(jsx);
+    // 获取当前页面的客户端 JSX 对象
+    const clientJSX = await renderJSXToClientJSX(jsx);
+    // 拼接到脚本代码中
+    const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
+    html += `<script>window.__INITIAL_CLIENT_JSX_STRING__ = `;
+    html += JSON.stringify(clientJSXString).replace(/</g, "\\u003c");
+    html += `</script>`;
+    html += `
+  <script type="importmap">
+    {
+      "imports": {
+        "react": "https://esm.sh/react@18.2.0",
+        "react-dom/client": "https://esm.sh/react-dom@18.2.0/client?dev"
+      }
+    }
+  </script>
+  <script type="module" src="/client.js"></script>
+`;
+    return html;
+}
+
+export async function jsxGenerator(url) {
+    let clientJSX = await renderJSXToClientJSX(<Router url={url} />);
+    const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
+    return clientJSXString
 }
 
 function Router({ url }) {
--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
 import express from "express";
-import { htmlGenerator } from "./generator";
+import {htmlGenerator, jsxGenerator} from "./generator";
+import {readFile} from "fs/promises";
+
 const app = express();
 
 app.get("/:route(*)", async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
-    const html = await htmlGenerator(url);
-    res.setHeader("Content-Type", "text/html");
-    res.end(html);
+
+    // 匹配 client.js
+    if (url.pathname === "/client.js") {
+        const content = await readFile("./client.js", "utf8");
+        res.setHeader("Content-Type", "text/javascript");
+        res.end(content);
+    }
+    // 如果网址有 jsx 参数，那就说明要获取 JSX 对象，我们改为调用 jsxGenerator 函数
+    else if (url.searchParams.has("jsx")) {
+        url.searchParams.delete("jsx");
+        const clientJSXString = await jsxGenerator(url);
+        res.setHeader("Content-Type", "application/json");
+        res.end(clientJSXString);
+    }
+    else {
+        const html = await htmlGenerator(url);
+        res.setHeader("Content-Type", "text/html");
+        res.end(html);
+    }
 });
 
 app.listen(3000, (err) => {
